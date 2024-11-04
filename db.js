63be24cbce904c524d3fff1ab903ddd82c794d5f
KJXSofttech <kjxsofttechpvtltd@gmail.com>
2024-11-04 12:09:35 +0530

    Added files of Portfolio

---
+++ db.js
@@ -0,0 +1,17 @@
+// Projects Database
+const data = [
+  {
+    gridArea: 'p1',
+    tabIndex: '8',
+    imageURL:
+      'images/projects/tecate-supremo-website/image.png',
+    imageAlt: 'Screenshoot of the performers section rendered with JavaScript',
+    title: 'Apna Weather',
+    tech: ['HTML/CSS', 'JavaScript','React JS'],
+    description:
+      'This project aims to show my skills on building a simple but light, fast, and easy to maintain website with technologies like React JS and JavaScript . With a fully responsive design and key features like dynamic rendering and transitions.',
+    liveLink: 'https://apnaweatherbyharsh.netlify.app',
+    sourceLink: 'https://github.com/Harsh5784/weather',
+  },
+];
+export default { data };
---
+++ scripts/works.js
@@ -0,0 +1,39 @@
+// Render #works content
+
+import projects from '../db.js';
+
+const projectData = projects.data;
+
+document.getElementById('works').innerHTML = `
+<h1 class="playfair" tabindex="7" aria-label="My projects:">
+  Projects
+</h1>
+${projectData.map((project) => `
+<article class="project ${project.gridArea}" tabindex="${project.tabIndex}">
+<img
+  class="project-image"
+  src="${project.imageURL}"
+  alt="${project.imageAlt}"
+/>
+<div class="project-description poppins">
+  <h1 class="project-title" aria-label="${project.title}.">
+    ${project.title}
+  </h1>
+  <ul
+    class="tech-container"
+    aria-label="Technologies:${project.tech.map((t, i, a) => (t[i] === a.length - 1 ? t : ` ${t}`))}."
+  >
+    ${project.tech.map((tech) => `<li class="tech-item">${tech}</li>`).join('')}
+  </ul>
+  <button
+    class="btnBlue seeProjBtn"
+    aria-label="Press enter now to see this project."
+  >
+    See this project
+  </button>
+</div>
+</article>
+`).join('')}
+<div class="center deco"></div>
+<div class="left deco"></div>
+<div class="right deco"></div>`;
